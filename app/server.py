e7716ccd9a0e1c2751e724248b9fd568f1ce18d8
Nhoral <mike.z.cook@gmail.com>
2019-01-28 11:46:10 -0800

    Swap classes and models

--- app/server.py
+++ app/server.py
@@ -8,10 +8,10 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1kjge2veVrajLmk93iOg2Xkx7iFp7ezK0'
+export_file_name = 'yoga_pose.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['warrior', 'down_dog', 'low_lunge', 'tree', 'up_dog']
 path = Path(__file__).parent
 
 app = Starlette()
